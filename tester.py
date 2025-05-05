50ad141cf6962abc79daaeee3d0fb7696cf83b07
andyL09 <alawrence15@cps.edu>
2025-05-05 10:39:25 -0500

    more testing

--- tester.py
+++ tester.py
@@ -4,3 +4,13 @@ import re
 lane_page = wikipedia.WikipediaPage("Lane Tech College Prep High School")
 lane_page.content
 print(lane_page.content)
+print("----------------------------------------------------------------")
+
+# Extract the first paragraph
+first_paragraph = lane_page.content.split("\n\n")[0]
+print(first_paragraph)
+print("----------------------------------------------------------------")
+# Extract all numbers in the article
+allNumbers = lane_page.content.split("/d+")
+
+#does not work???? figure it out later
